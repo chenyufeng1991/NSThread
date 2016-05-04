70a52e804f83cc3071ba8abcbc0dfae69908cd81
chenyufeng <503434355@qq.com>
2016-05-04 20:07:49 +0800

    测试在子线程中调用主线程更新UI

--- NSThreadDemo/ThirdViewController.m
+++ NSThreadDemo/ThirdViewController.m
@@ -26,7 +26,23 @@
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    
 
+    NSThread *subThread = [[NSThread alloc] initWithTarget:self selector:@selector(run) object:nil];
+    [subThread start];
 }
+
+- (void)run
+{
+    NSLog(@"主线程1：%@，当前线程1：%@",[NSThread mainThread],[NSThread currentThread]);
+    //以下方法需要在子线程中调用
+    [self performSelectorOnMainThread:@selector(invocationMainThread) withObject:nil waitUntilDone:YES];
+}
+
+- (void)invocationMainThread
+{
+    NSLog(@"主线程2：%@，当前线程2：%@",[NSThread mainThread],[NSThread currentThread]);
+    NSLog(@"调用主线程更新UI");
+}
+
+
 @end
